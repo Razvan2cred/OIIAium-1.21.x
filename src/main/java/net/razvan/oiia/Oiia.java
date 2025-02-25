c884e1c5573cd353bf37a3f2625f8f49bb9533b0
Razvan-Unitbv <razvan.calancea@student.unitbv.ro>
2025-02-26 01:02:37 +0200

    Oiia Items and Blocks Creative Mode Tabs

--- src/main/java/net/razvan/oiia/Oiia.java
+++ src/main/java/net/razvan/oiia/Oiia.java
@@ -2,6 +2,7 @@ package net.razvan.oiia;
 
 import net.minecraft.world.item.CreativeModeTabs;
 import net.razvan.oiia.block.ModBlocks;
+import net.razvan.oiia.item.ModCreativeModeTabs;
 import net.razvan.oiia.item.ModItems;
 import org.slf4j.Logger;
 
@@ -36,6 +37,8 @@ public class Oiia {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
---
+++ src/main/java/net/razvan/oiia/item/ModCreativeModeTabs.java
@@ -0,0 +1,39 @@
+package net.razvan.oiia.item;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.razvan.oiia.Oiia;
+import net.razvan.oiia.block.ModBlocks;
+
+import java.util.function.Supplier;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Oiia.MOD_ID);
+
+    public static final Supplier<CreativeModeTab> OIIA_ITEMS_TAB = CREATIVE_MODE_TAB.register("oiia_items_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.OIIAITE.get()))
+                    .title(Component.translatable("creativetab.oiiamod.oiia_items"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.OIIAITE);
+                        output.accept(ModItems.RAW_OIIAITE);
+                    }).build());
+    public static final Supplier<CreativeModeTab> OIIA_BLOCKS_TAB = CREATIVE_MODE_TAB.register("oiia_blocks_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModBlocks.OIIAITE_BLOCK.get()))
+                    .withTabsBefore(ResourceLocation.fromNamespaceAndPath(Oiia.MOD_ID, "oiia_items_tab"))
+                    .title(Component.translatable("creativetab.oiiamod.oiia_blocks"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModBlocks.OIIAITE_BLOCK);
+                        output.accept(ModBlocks.OIIAITE_ORE);
+                    }).build());
+    public static void register(IEventBus eventBus){
+        CREATIVE_MODE_TAB.register(eventBus);
+    }
+
+
+}
