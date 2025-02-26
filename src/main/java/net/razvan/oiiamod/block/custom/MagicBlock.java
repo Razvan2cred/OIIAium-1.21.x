8225ea62caaf5635ff667ae17d0d7bf9a8f1b192
Razvan-Unitbv <razvan.calancea@student.unitbv.ro>
2025-02-26 17:21:23 +0200

    Oiiator recipe

---
+++ src/main/java/net/razvan/oiiamod/block/custom/MagicBlock.java
@@ -0,0 +1,34 @@
+package net.razvan.oiiamod.block.custom;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+
+public class MagicBlock extends Block {
+    public MagicBlock(Properties properties) {
+        super(properties);
+    }
+    //ctrl h pe block pt inspiratie
+
+    @Override
+    protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos,
+                                               Player player, BlockHitResult hitResult) {
+        level.playSound(player, pos, SoundEvents.CAT_BEG_FOR_FOOD, SoundSource.BLOCKS, 1f, 1f);
+        return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void stepOn(Level level, BlockPos pos, BlockState state, Entity entity) {
+
+
+
+        super.stepOn(level, pos, state, entity);
+    }
+}
