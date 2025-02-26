22720901f6f68a00953e38e02dbacc41cd591036
Razvan-Unitbv <razvan.calancea@student.unitbv.ro>
2025-02-27 00:58:32 +0200

    Cat poop food item

--- src/main/java/net/razvan/oiiamod/item/ModItems.java
+++ src/main/java/net/razvan/oiiamod/item/ModItems.java
@@ -5,6 +5,7 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.razvan.oiiamod.Oiia;
+import net.razvan.oiiamod.item.custom.FuelItem;
 import net.razvan.oiiamod.item.custom.OiiaItem;
 
 public class ModItems {
@@ -18,6 +19,12 @@ public class ModItems {
 
     public static final DeferredItem<Item> OIIATOR = ITEMS.register("oiiator",
             () -> new OiiaItem(new Item.Properties().durability(32)));
+    public static final DeferredItem<Item> CAT_POOP = ITEMS.register("cat_poop",
+            () -> new Item(new Item.Properties().food(ModFoodProperties.CAT_POO)));
+    public static final DeferredItem<Item> DEAD_CAT = ITEMS.register("dead_cat",
+            () -> new FuelItem(new Item.Properties(), 1000));
+    public static final DeferredItem<Item> HALF_DEAD_CAT = ITEMS.register("half_dead_cat",
+            () -> new Item(new Item.Properties())); //metoda 2, in neoforge/data_maps/item/furnace_fuels.json!!!
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
