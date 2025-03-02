e4395c39e80ea1597a163dabc3ca9e3c7cd8dcf2
Razvan-Unitbv <razvan.calancea@student.unitbv.ro>
2025-03-03 00:40:09 +0200

    End Rod sheep hitting and Villager abuse events.

--- src/main/java/net/razvan/oiiamod/event/ModEvents.java
+++ src/main/java/net/razvan/oiiamod/event/ModEvents.java
@@ -1,11 +1,18 @@
 package net.razvan.oiiamod.event;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.animal.Sheep;
+import net.minecraft.world.entity.npc.Villager;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
 import net.neoforged.neoforge.event.level.BlockEvent;
 import net.razvan.oiiamod.Oiia;
 import net.razvan.oiiamod.item.custom.HammerItem;
@@ -39,6 +46,21 @@ public class ModEvents {
                 serverPlayer.gameMode.destroyBlock(pos);
                 HARVESTED_BLOCKS.remove(pos);
             }
+        }
+    }
+@SubscribeEvent
+    public static void livingDamage(LivingDamageEvent.Pre event) {
+        if(event.getEntity() instanceof Sheep sheep && event.getSource().getDirectEntity() instanceof Player player){
+            if(player.getMainHandItem().getItem() == Items.END_ROD) {
+                player.sendSystemMessage(Component.literal(player.getName().getString() + "just raped a sheep"));
+                sheep.addEffect(new MobEffectInstance(MobEffects.POISON, 100, 3));
+                player.getMainHandItem().shrink(1);
+            }
+        }
+        if(event.getEntity() instanceof Villager villager && event.getSource().getDirectEntity() instanceof Player player){
+            player.sendSystemMessage(Component.literal(player.getName().getString() + " touched a villager's intimate area."));
+            villager.addEffect(new MobEffectInstance(MobEffects.LEVITATION,100,3));
+
         }
     }
 }
