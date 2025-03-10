a87097d23d36a582741a1c0442410ea1152be9f5
Razvan-Unitbv <razvan.calancea@student.unitbv.ro>
2025-03-11 00:02:24 +0200

    Oiia Cat Natural Spawn Plains + End Islands

--- src/main/java/net/razvan/oiiamod/event/ModEventBusEvents.java
+++ src/main/java/net/razvan/oiiamod/event/ModEventBusEvents.java
@@ -1,9 +1,14 @@
 package net.razvan.oiiamod.event;
 
+import net.minecraft.world.entity.SpawnPlacementType;
+import net.minecraft.world.entity.SpawnPlacementTypes;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
+import net.neoforged.neoforge.event.entity.RegisterSpawnPlacementsEvent;
 import net.razvan.oiiamod.Oiia;
 import net.razvan.oiiamod.entity.ModEntities;
 import net.razvan.oiiamod.entity.client.OiiaCatModel;
@@ -19,5 +24,10 @@ public class ModEventBusEvents {
     @SubscribeEvent
     public static void registerAttributes(EntityAttributeCreationEvent event){
         event.put(ModEntities.OIIA_CAT.get(), OiiaCatEntity.createAttributes().build());
+    }
+    @SubscribeEvent
+    public static void registerSpawnPlacements(RegisterSpawnPlacementsEvent event) {
+        event.register(ModEntities.OIIA_CAT.get(), SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
     }
 }
--- src/main/java/net/razvan/oiiamod/worldgen/ModBiomeModifiers.java
+++ src/main/java/net/razvan/oiiamod/worldgen/ModBiomeModifiers.java
@@ -7,11 +7,17 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BiomeTags;
 import net.minecraft.world.level.biome.Biomes;
+import net.minecraft.world.level.biome.MobSpawnSettings;
 import net.minecraft.world.level.levelgen.GenerationStep;
 import net.neoforged.neoforge.common.world.BiomeModifier;
 import net.neoforged.neoforge.common.world.BiomeModifiers;
+import net.neoforged.neoforge.common.world.MobSpawnSettingsBuilder;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
 import net.razvan.oiiamod.Oiia;
+import net.razvan.oiiamod.entity.ModEntities;
+
+import java.util.List;
+
 
 public class ModBiomeModifiers {
     public static final ResourceKey<BiomeModifier> ADD_OIIAITE_ORE = registerKey("add_oiiaite_ore");
@@ -19,6 +25,7 @@ public class ModBiomeModifiers {
     public static final ResourceKey<BiomeModifier> ADD_END_OIIAITE_ORE = registerKey("add_end_oiiaite_ore");
     public static final ResourceKey<BiomeModifier> ADD_TREE_TECHWOOD = registerKey("add_tree_techwood");
     public static final ResourceKey<BiomeModifier> ADD_SIGMA_BERRY_BUSH = registerKey("add_sigma_berry_bush");
+    public static final ResourceKey<BiomeModifier> SPAWN_OIIA_CAT = registerKey("spawn_oiia_cat");
 
     public static void bootstrap(BootstrapContext<BiomeModifier> context) {
         // CF -> PF -> BM
@@ -53,6 +60,10 @@ public class ModBiomeModifiers {
                 HolderSet.direct(biomes.getOrThrow(Biomes.PLAINS), biomes.getOrThrow(Biomes.SNOWY_PLAINS)),
                 HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.SIGMA_BERRY_BUSH_PLACE_KEY)),
                 GenerationStep.Decoration.VEGETAL_DECORATION));
+
+        context.register(SPAWN_OIIA_CAT, new BiomeModifiers.AddSpawnsBiomeModifier(
+                HolderSet.direct(biomes.getOrThrow(Biomes.PLAINS), biomes.getOrThrow(Biomes.SMALL_END_ISLANDS)),
+                List.of(new MobSpawnSettings.SpawnerData(ModEntities.OIIA_CAT.get(), 20, 2, 4))));
 
     }
     private static ResourceKey<BiomeModifier> registerKey(String name) {
